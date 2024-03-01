a439ee1169796e92354fd94972857bcd4daa46fd
Nelson Lara <nelsonlarajr@gmail.com>
2024-02-29 23:17:28 -0300

    staticfiles

--- gomenu/urls.py
+++ gomenu/urls.py
@@ -1,20 +1,3 @@
-"""gomenu URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -28,10 +11,4 @@ urlpatterns = [
     path('menus/', include('menus.urls')),
     path('orders/', include('orders.urls')),
     path('', include('restaurants.urls')),
-] #+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-# if settings.DEBUG:
-#     import debug_toolbar
-#     urlpatterns = [
-#         path('__debug__', include(debug_toolbar.urls)),
-#     ] + urlpatterns
+] 
